63f2afad01cd1abb09e55a05e356b7f30e4c8293
johndoe2196 <johndoe219656@gmail.com>
2020-11-21 18:04:04 +0000

    updated registration page so that it an api key is now automatically generated and shown to the user

--- register/index.js
+++ register/index.js
@@ -1,6 +1,32 @@
 const form = document.querySelector('form');
 
 const API_URL = 'https://whispering-river-16276.herokuapp.com/api/user/register';
+const genKey_URL = ' https://autotune-api-key-generator.herokuapp.com/'
+
+
+
+ makeGenKeyAPIrequest = async function(user){
+   
+    let response = await fetch(genKey_URL, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'content-type': 'application/json',
+            "password" : user.password,
+            "email": user.email
+        }
+    })
+    .then(status)
+    .then(res => res.json())
+    .then(res => localStorage.setItem("apikey", res.userAPIKEY))
+    }
+    
+    function status(res) {
+        if (!res.ok) {
+            return Promise.reject();
+        }
+        return res;
+    }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -10,14 +36,14 @@ form.addEventListener('submit', (event) => {
     const password = formData.get('password');
     console.log('form submitted')
 
-    const user = {
+    localStorage.removeItem("apikey");
+
+    var user = {
         "email": email, 
         "name": name,
         "password": password
     };
     console.log(user)
-
-    var res;
 
     fetch(API_URL, {
         method: 'POST',
@@ -27,18 +53,26 @@ form.addEventListener('submit', (event) => {
           }
     })
     .then(response => {
+        
         if(!response.ok){
             alert("Invalid request. Ensure username and password are longer than 6 characters and email is not already registered to an Autotune account")
             throw new Error('Invalid Request')
         }else{
-            form.reset();
-            window.location.href = "../index.html"
+            genKeyFunction(user)
+        
         }
     })
     .catch((error) => {
         console.log(error);
     });
 });
+
+genKeyFunction = async function(user){
+    const keyAPI = await makeGenKeyAPIrequest(user)
+    console.log(localStorage.getItem("apikey"));
+    form.reset();
+    window.location.href = "genkey.html"
+}
 
 
 
